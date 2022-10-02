5fc44d6f0ac84e93600c64d15784446b7e30d492
unknown <jec.dev.22@gmail.com>
2022-10-02 17:20:49 -0300

    [Add] Assets

---
+++ src/assets/imageUrl.ts
@@ -0,0 +1,20 @@
+import logoImg from './logo.png'
+import linkedinImg from './linkedin.png'
+import notFoundImg from './not-found.jpg'
+import magnifyingGlassImg from './magnifying-glass.png'
+import shoppingCartImg from './shopping-cart.png'
+
+// ----------------------------------------------
+
+import menuForMobileImg from './menu-for-mobile.png'
+
+// ----------------------------------------------
+
+export {
+  logoImg,
+  linkedinImg,
+  notFoundImg,
+  magnifyingGlassImg,
+  shoppingCartImg,
+  menuForMobileImg
+}
