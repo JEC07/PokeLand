a4a8901d8f21dd53d27a87930691320b649e2178
unknown <jec.dev.22@gmail.com>
2022-10-07 22:51:06 -0300

    [Add] Nuevas funcionalidades
    Al hacer peticiones a la API, no hay un seguimiento sobre el progreso de los datos.

--- src/hooks/usePokemons.ts
+++ src/hooks/usePokemons.ts
@@ -1,33 +1,93 @@
 import { useEffect, useState } from 'react'
 import { getPokemonByName } from '../services/pokemonService'
 import { removeUndefined } from '../tools/array'
-import { Pokemon, PokemonAbility, PokemonSearch, PokemonType } from '../typescript/types'
+import { Pokemon, PokemonAbility, PokemonSearch, PokemonType, QueryResponse, Search } from '../typescript/types'
 
 type PokemonByAbilityOrType = PokemonType | PokemonAbility
+type FunctionType = 'getPokemonsByAbilityOrType' | 'getPokemon' | 'getPokemons'
 
 export const usePokemons = (
-  searchPokemon: string | undefined,
-  callback: (searchPokemon: string) => Promise<PokemonByAbilityOrType | undefined>
+  functionType: FunctionType,
+  search?: string,
+  searchList?: QueryResponse,
+  callback?: (search: string) => Promise<PokemonByAbilityOrType | undefined>
 ) => {
   const [pokemonsList, setPokemonsList] = useState<Pokemon[]>([])
+  const [pokemon, setPokemon] = useState<Pokemon>()
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [error, setError] = useState<boolean>(false)
 
   useEffect(() => {
-    if (searchPokemon) {
-      const getPokemons = async () => {
-        const data: PokemonByAbilityOrType | undefined = await callback(searchPokemon)
+    if (search || searchList) {
+      setIsLoading(true)
+      setError(false)
 
-        if (data) {
-          const response: (Pokemon | undefined)[] = await Promise.all(
-            data.pokemon.map((value: PokemonSearch) => getPokemonByName(value.pokemon.name))
-          )
-
-          setPokemonsList(removeUndefined<Pokemon>(response))
-        }
-      }
-
-      getPokemons()
+      if (functionType === 'getPokemonsByAbilityOrType') getPokemonsByAbilityOrType()
+      if (functionType === 'getPokemon') getPokemon()
+      if (functionType === 'getPokemons') getPokemons()
     }
-  }, [searchPokemon, callback])
+  }, [functionType, search, searchList])
 
-  return pokemonsList
+  const getPokemonsByAbilityOrType = async () => {
+    if (search && callback) {
+      const data = await callback(search)
+
+      if (data && (data.pokemon.length > 0)) {
+        const response: (Pokemon | undefined)[] = await Promise.all(
+          data.pokemon.map((value: PokemonSearch) => getPokemonByName(value.pokemon.name))
+        )
+
+        setPokemonsList(removeUndefined<Pokemon>(response))
+        return resetByDefault()
+      }
+    }
+
+    return resetByDefaultWithError()
+  }
+
+  const getPokemon = async () => {
+    if (search) {
+      const data = await getPokemonByName(search)
+
+      if (data) {
+        setPokemon(data)
+        return resetByDefault()
+      }
+    }
+
+    return resetByDefaultWithError()
+  }
+
+  const getPokemons = async () => {
+    if (searchList && searchList.results.length > 0) {
+      const newList = searchList.results.map((value: Search) => value.name)
+      const promiseList = newList.map((value: string) => getPokemonByName(value))
+      const data = await Promise.all(promiseList)
+      const newData = removeUndefined<Pokemon>(data)
+
+      if (newData.length > 0) {
+        setPokemonsList(newData)
+        return resetByDefault()
+      }
+    }
+
+    return resetByDefaultWithError()
+  }
+
+  const resetByDefaultWithError = () => {
+    setError(true)
+    setIsLoading(false)
+  }
+
+  const resetByDefault = () => {
+    setError(false)
+    setIsLoading(false)
+  }
+
+  return {
+    pokemonsList,
+    pokemon,
+    isLoading,
+    error
+  }
 }
